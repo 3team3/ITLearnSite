760409c8317122c0238a25d9d7807e3762757caa
SsackTeun <jaeyoung645@naver.com>
2020-01-05 16:15:19 +0900

    commit

--- Copy of ITLearnSite/WebContent/js/commons.js
+++ Copy of ITLearnSite/WebContent/js/commons.js
@@ -1,5 +1,12 @@
 var ctx = window.location.pathname.substring(0, window.location.pathname.indexOf("/", 2));
-
+var //status
+chkEmail = false
+, chkPw1 = false
+, chkPw2 = false
+, chkName = false 
+, chkPhone = false
+, chkGender = false
+, chkAddr = false;
 /*회원가입 유효성*/
 
 /*이메일(아이디) 유효성 체크 */
@@ -12,6 +19,7 @@ function emailDupChk(){
 	{
 		$("#email ~ p").remove();
 		$("#email").after("<p style = 'color : red; '> 올바른 형식이 아닙니다 </p>");
+		chkEmail = false;
 	}
 	else
 	{
@@ -32,12 +40,14 @@ function emailDupChk(){
 					$("#email ~ p").remove();
 					$("#email").after("<p style = 'color : red; '> 중복 </p>");
 					console.log("중복");
+					chkEmail = false;
 				}
 				if(getData == 0)
 				{
 					$("#email ~ p").remove();
 					$("#email").after("<p style = 'color : green; '> 사용가능 </p>");
 					console.log("사용가능");
+					chkEmail = true;
 				}
 			}
 		});
@@ -53,11 +63,13 @@ function pwdRegChk(){
 	{
 		$("#pw1 ~ p").remove();
 		$("#pw1").after("<p style = 'color : red; '> 숫자, 특수문자 1회이상 , 영문 2개 이상 8자리 이상 입력 </p>");
+		chkPw1 = false;
 	}
 	else
 	{
 		$("#pw1 ~ p").remove();
 		$("#pw1").after("<p style = 'color : green; '> 사용가능 </p>");
+		chkPw1 = true;
 	}
 }
 
@@ -69,11 +81,13 @@ function pwdEqualChk(){
 	{
 		$("#pw2 ~ p").remove();
 		$("#pw2").after("<p style = 'color : green; '> 일치함 </p>");
+		chkPw2 = true;
 	}
 	else
 	{
 		$("#pw2 ~ p").remove();
 		$("#pw2").after("<p style = 'color : red; '> 일치하지 않음 </p>");
+		chkPw2 = false;
 	}
 }
 
@@ -85,17 +99,108 @@ function nameChk(){
 	{
 		$("#name ~ p").remove();
 		$("#name").after("<p style = 'color : red;'> 이름은 2~4자까지 입니다</p>");
+		chkName = false;
 	}
 	else
 	{
 		$("#name ~ p").remove();
 		$("#name").after("<p style = 'color : green;'> 사용가능 </p>");
+		chkName = true;
 	}
 }
 
-function birthChk(){
+function selectGenderChk(){
+	var genChk = $("#gender").val();
+	
+	if(genChk == 1)
+	{
+		chkGender = true;
+	}
+	else if(genChk == 2)
+	{
+		chkGender = true;
+	}
+	else
+	{
+		chkGender = false;
+	}
 	
 }
+
+function phoneChk(){
+	var phonenumber = $("#phonenumber").val();
+	var phonePattern = /^[0-9]*$/;
+	
+	if(!phonePattern.test(phonenumber))
+	{
+		alert("유효하지 않은 휴대폰번호입니다.");
+		$("#phonenumber").val("");
+		chkPhone = false;
+	}
+	else
+	{
+		chkPhone = true;
+	}
+}
+
+function addressChk(){
+	var addr = $("#address").val();
+	var addr1 = $("#address1").val();
+	var addr2 = $("#address2").val();
+	
+	if(addr == null)
+	{
+		chkAddr = false;
+	}
+	else if(addr1 == null)
+	{
+		chkAddr = false;
+	}
+	else if(addr2 == null)
+	{
+		chkAddr = false;
+	}
+	else
+	{
+		chkAddr = true;
+	}
+}
+
+
+function submitter(){
+	if(chkEmail != true)
+	{
+		$("#email").focus();
+		return false;
+	}
+	else if(chkPw1 != true)
+	{
+		$("#email").focus();
+		return false;
+	}
+	else if(chkPw2 != true)
+	{
+		return false;
+	}
+	else if(chkName != true)
+	{
+		return false;
+	}
+	else if(chkGender != true)
+	{
+		return false;
+	}
+	else if(chkAddr != true)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+
 
 
 
