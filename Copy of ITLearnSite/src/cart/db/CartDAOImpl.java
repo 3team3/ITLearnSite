29a31d1dd60ca3f5f56d4204198e070644c4b2d7
HaEuny <59347898+HaEuny@users.noreply.github.com>
2020-01-20 15:12:39 +0900

    장바구니 리스트 불러오기

--- Copy of ITLearnSite/src/cart/db/CartDAOImpl.java
+++ Copy of ITLearnSite/src/cart/db/CartDAOImpl.java
@@ -5,6 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -36,5 +38,34 @@ public class CartDAOImpl implements CartDAO {
             System.out.println("closeConnection()메소드에서 오류  : " +e);
         }
     }
+
+	public ArrayList<CartBean> getcartlist(String email) {
+		ArrayList<CartBean> cartlist=new ArrayList<CartBean>();
+		try{
+			con=getConnection();
+			sql="select * from cart where email=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, email);
+			rs=pstmt.executeQuery();
+			while(rs.next()){
+				CartBean cbean = new CartBean();
+				cbean.setCart_num(rs.getInt("cart_num"));
+				cbean.setEmail(rs.getString("email"));
+				cbean.setPro_name(rs.getString("pro_name"));
+				cbean.setPro_cnt(rs.getInt("pro_cnt"));
+				cbean.setPro_price(rs.getInt("pro_price"));
+				cbean.setPro_sort(rs.getString("pro_sort"));
+				cartlist.add(cbean);
+			}
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			
+		}finally{
+        closeConnection();  
+		}		
+		return cartlist;
+	}
+
 }
 
--- Copy of ITLearnSite/src/cart/service/CartServiceImpl.java
+++ Copy of ITLearnSite/src/cart/service/CartServiceImpl.java
@@ -1,5 +1,24 @@
 package cart.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cart.db.CartBean;
+import cart.db.CartDAOImpl;
+
 public class CartServiceImpl implements CartService{
+CartDAOImpl dao;
+
+public CartServiceImpl(){
+	dao= new CartDAOImpl();
+	
+}
+	
+	
+	public ArrayList<CartBean> getcartlist(String email) {
+		System.out.println("cartlist service");	
+		ArrayList<CartBean> cartlist=dao.getcartlist(email);
+		return cartlist; 
+	}
 
 }
